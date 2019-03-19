583b9801ae4c1f44034c807f32be7ad9b1506f87
Abdul <awsayed@alaska.edu>
2019-03-19 01:04:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n) {
+  let ranked = document.querySelectorAll('ul.ranked-list li');
+    for (let i=0; i<ranked.length; i++) {
+        ranked[i].innerHTML = (parseInt(ranked[i].innerHTML) + n).toString();
+    }
+}
+
+
+function deepestChild() {
+  let nested = document.getElementById('grand-node').querySelectorAll('div');
+  return nested[nested.length - 1];
+}
+
 
