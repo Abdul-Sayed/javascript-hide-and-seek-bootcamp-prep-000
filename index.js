d1a76dc67d1c20c236169564dd8118d9f12938e1
Abdul <awsayed@alaska.edu>
2019-03-19 01:00:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// 
+function getFirstSelector(selector) {
+  return document.querySelector('selector');
+}
+
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n) {
+    for (let i=0; i<ranked.length; i++) {
+        ranked[i].innerHTML = (parseInt(ranked[i].innerHTML) + n).toString();
+    }
+}
+
+
+function deepestChild() {
+  let nested = document.getElementById('grand-node').querySelectorAll('div');
+  return nested[nested.length - 1].textContent;
+}
+
 
